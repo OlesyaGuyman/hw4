2634c49c9d74264ea46532577a27f0225723ace7
OlesyaGuyman <olesya.guyman@yandex.ru>
2022-12-09 20:35:53 +0300

    add all

---
+++ 25/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B. 
+
+3, 5 -> 243 (3⁵)
+2, 4 -> 16 */
+
+Console.Write("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int AtoBDegree(int ac, int bc)
+{
+    int result = 1;
+    for (int i = 0; i < bc; i++)
+    {
+        result = result * ac;
+    }
+    return result;
+}
+
+int aToBDegree = AtoBDegree(a, b);
+
+Console.WriteLine($"{a} в степени {b} равно {aToBDegree}.");
---
+++ 27/Program.cs
@@ -0,0 +1,32 @@
+﻿/* // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+int SumDigits(int num)
+{
+    int res = num % 10;
+    if (num > 0)
+    {
+        for (int i = 0; num / 10 > 0; i++)
+        {
+            num = num / 10;
+            res = res + num % 10;
+        }
+        return res;
+    }
+    else
+    {
+        Console.WriteLine("Введите положительное число");
+        return 0;
+    }
+}
+
+int sumDigits = SumDigits(n);
+
+Console.WriteLine($"Сумма цифр из {n} равна {sumDigits}.");
